d40c2c3e4da6c01c0b9a92f07ba9606007bcadf1
Josiah Liciaga <35929809+Jliciaga@users.noreply.github.com>
2018-07-30 16:15:29 -0400

    caps change in index.js

--- src/app/index.js
+++ src/app/index.js
@@ -21,7 +21,7 @@ import ModalSignupForm from './signup.js';
 import ModalLoginForm from './login.js';
 import SearchApiForm from './searchApi.jsx';
 import RecipesList from './recipes.js';
-import RecipeDetails from './RecipeDetails.js';
+import RecipeDetails from './recipeDetails.js';
 import MealView from './mealView.js';
 import data from './data.js';
 import $ from 'jquery';
